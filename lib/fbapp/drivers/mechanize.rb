034631fd26dc21bec78deba6bfd2e26c70d1e14c
Ben Marini <ben.marini@akqa.com>
2010-10-24 12:51:36 -0700

    Allow `fbapp show` to accept an id or name

--- lib/fbapp/drivers/mechanize.rb
+++ lib/fbapp/drivers/mechanize.rb
@@ -3,21 +3,29 @@ require 'mechanize'
 module Fbapp
   module Drivers
     class Mechanize < Base
+
+      App = Struct.new(:app_id, :name)
+
       def list
         login
         page = browser.get("http://www.facebook.com/developers/apps.php")
-        page.search("#dev_app_list a").each do |app|
-          puts " * " + app.text
+        apps = fetch_apps(page)
+
+        apps.each do |app|
+          puts " * %s %s" % [app.app_id, app.name]
         end
       end
 
       def show(appname)
         login
         page = browser.get("http://www.facebook.com/developers/apps.php")
-        link = page.search("#dev_app_list a").find { |a| a.text == appname }
 
-        id  = link.parent["id"].split("_").first
-        dl  = page.search("##{id}_info .dev_application_info dl")
+        id = begin
+          apps = fetch_apps(page)
+          apps.find { |a| a.name == appname || a.app_id == appname }.app_id
+        end
+
+        dl = page.search("##{id}_info .dev_application_info dl")
 
         titles, values = dl.search("dt"), dl.search("dd")
         titles = titles.to_a.map { |t| t.text }
@@ -46,6 +54,15 @@ module Fbapp
         browser.submit(form)
       end
 
+      # Assumes page is http://www.facebook.com/developers/apps.php
+      def fetch_apps(page)
+        page.search("#dev_app_list a").collect do |a|
+          id   = a.parent["id"].split("_").first
+          name = a.text
+
+          App.new(id, name)
+        end
+      end
     end
   end
 end
