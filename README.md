bfa9c621f3b166b86afb7ef12495139941dee098
Ben Marini <bmarini@gmail.com>
2010-11-22 08:10:07 -0800

    Refactor a bit

--- README.md
+++ README.md
@@ -8,3 +8,8 @@ So far you can only do two things:
 
     fbapp list         # List your facebook apps
     fbapp show         # Show app settings
+
+## Caveats
+
+This will store your facebook email and password unencrypted in ~/.fbapp
+TODO: Use `security` to access the Keychain on a Mac.
--- lib/fbapp/drivers/base.rb
+++ lib/fbapp/drivers/base.rb
@@ -5,21 +5,34 @@ module Fbapp
     class Base
 
       def get_or_cache_credentials
-        credfile = File.expand_path("~/fbapp")
-        if File.exist?(credfile)
-          email, pass = File.read(credfile).split("\n")
-        else
-          highline = HighLine.new
-          email    = highline.ask("Email:    ")
-          pass     = highline.ask("Password: ") { |q| q.echo = false }
+        credfile_exists? ? creds_from_file : creds_from_ui
+      end
 
-          File.open(credfile, "w+") do |f|
-            f.puts email
-            f.puts pass
-          end
+      protected
+
+      def creds_from_file
+        File.read(credfile).split("\n")
+      end
+
+      def creds_from_ui
+        highline = HighLine.new
+        email    = highline.ask("Email:    ")
+        pass     = highline.ask("Password: ") { |q| q.echo = false }
+
+        File.open(credfile, "w+") do |f|
+          f.puts email
+          f.puts pass
         end
 
-        return [email, pass]
+        return [ email, pass ]
+      end
+
+      def credfile_exists?
+        File.exist?(credfile)
+      end
+
+      def credfile
+        File.expand_path("~/.fbapp")
       end
 
     end
